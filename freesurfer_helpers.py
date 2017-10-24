d18de3c9f653fe5a18cc4521abd4ab0ed966a944
leej3 <johnleenimh@gmail.com>
2017-10-24 14:43:03 -0400

    finish up fs helpers

--- freesurfer_helpers.py
+++ freesurfer_helpers.py
@@ -1,6 +1,11 @@
 # coding: utf-8
 
 from pathlib import Path
+
+def make_swarm_path(directory,recon, analysis_version):
+    directory = Path(directory)
+    return directory.joinpath(recon + '_' + analysis_version + '.cmd')
+
 def generate_freesurfer_command(df_row=None,output_dir=None,ncpus='4',conf_script=None,clustered_directive='all',image_col='scan_path',expert_opts=None,longitudinal=False):
     tpNid = df_row.subject + '_' + df_row.ses
     
@@ -21,10 +26,6 @@ def generate_freesurfer_command(df_row=None,output_dir=None,ncpus='4',conf_scrip
         cmd = cmd + ' -s ' + tpNid
     return cmd
 
-def make_swarm_path(directory,recon, analysis_version):
-    directory = Path(directory)
-    return directory.joinpath(recon + '_' + analysis_version + '.cmd')
-
 def generate_recon_swarm(df,recon,clust,outdir,conf_script,ncpus,freesurfer_dir,analysis_version,expert_opts,longitudinal=False):
     df[recon] = (
         df.
@@ -36,7 +37,7 @@ def generate_recon_swarm(df,recon,clust,outdir,conf_script,ncpus,freesurfer_dir,
                                         conf_script = conf_script,
                                         clustered_directive = clust,
                                         ncpus = ncpus,
-                                        expert_opts = expert_opts
+                                        expert_opts = expert_opts,
                                        longitudinal = longitudinal),
         axis = 1)
         )
@@ -44,3 +45,31 @@ def generate_recon_swarm(df,recon,clust,outdir,conf_script,ncpus,freesurfer_dir,
         
     swm_file.write_text('\n'.join(df[recon])) 
     return (df, swm_file)
+
+def generate_template_command(df=None,output_dir=None,conf_script=None,expert_opts=None):
+# command to generate:    recon-all -base <templateid> -tp <tp1id> -tp <tp2id> ... -all
+    sub_ids = set(df.subject)
+    assert len(sub_ids) == 1
+    sub_id = list(sub_ids)[0]
+
+    cmd = 'source ' + conf_script.as_posix() + ';' + \
+    ' recon-all' +  \
+    ' -base' + \
+    ' ' + sub_id + '_template' + \
+    ' -tp ' + ' -tp '.join(df.subject + '_' + df.ses) + \
+    ' -hires' + \
+    ' -expert ' + expert_opts.as_posix() +  ' -xopts-overwrite' + \
+    ' -all' 
+
+    return cmd
+# generate_template_command(df, outdir, subj_dir, conf_script)
+
+
+def generate_subfield_command(df=None,conf_script=None,expert_opts=None):
+    # (cross sectional):  recon-all -all -s <tpNid> -i path_to_tpN_dcm
+    # longitudinal command:  recon-all -long <tpNid> <templateid> -all  
+    # command to generate: longHippoSubfieldsT1.sh <baseID> [SubjectsDirectory]
+    cmd = 'source ' + conf_script.as_posix() + ';' + \
+    ' longHippoSubfieldsT1.sh' + \
+    ' ' + df.subject + '_template'
+    return cmd
